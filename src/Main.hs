1b8ff84add59452c7a40b24472bea3e82d362fc1
George Thomas <georgefsthomas@gmail.com>
2020-10-26 11:24:46 +0000

    Mirror Rust implementation
    In particular, use AltGr as a toggle, rather than requiring it to be held.

--- src/Main.hs
+++ src/Main.hs
@@ -4,10 +4,12 @@ import Control.Monad
 import Data.Bifunctor
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as C
+import Data.Generics.Labels ()
 import Data.List.Split
 import Evdev
 import Evdev.Codes
 import Evdev.Stream
+import Lens.Micro
 import Network.Socket
 import Network.Socket.ByteString
 import Options.Generic
@@ -15,6 +17,10 @@ import Streamly
 import qualified Streamly.Prelude as S
 import System.Process
 
+{-TODO mirror Rust args
+currently we always start idle and switch with right alt
+device arg is irrelevant as here we always read from all
+-}
 data Args = Args
     { port :: Int
     , ip :: String
@@ -28,36 +34,60 @@ main = do
     sock <- socket AF_INET Datagram defaultProtocol
     bind sock $ SockAddrInet defaultPort 0
     let addr = SockAddrInet (fromIntegral $ port args) $ readIp $ ip args
-    void $ S.foldlM' (uncurry . f sock addr) False $ S.map (second eventData) allEvs
+        s =
+            State
+                { active = False -- currently grabbed and sending events
+                , interrupted = False -- have there been any events from other keys since switch was last pressed?
+                , hangingSwitch = False -- we don't necessarily want to send a switch down event, since we might actually
+                -- be switching mode - so we carry over to the next round
+                }
+    void $ S.foldlM' (uncurry . f sock addr) s $ S.map (second eventData) allEvs
 
-type State = Bool
+data State = State
+    { active :: Bool
+    , interrupted :: Bool
+    , hangingSwitch :: Bool
+    }
+    deriving (Generic)
 
+--TODO use State monad
 f :: Socket -> SockAddr -> State -> Device -> EventData -> IO State
-f sock addr active dev = \case
-    KeyEvent KeyRightalt t ->
-        case t of
-            Pressed -> do
-                {-TODO the disabling isn't quite gonna work for bluetooth keyboard, with several devices in one
-                    only disable the device which pressed altgr -}
-                n <- deviceName dev --TODO cache (although tbh, while it may be IO, it should still be very cheap)
-                callProcess "xinput" ["disable", C.unpack n] --TODO ByteString would be nice
-                -- grabDevice dev -- this confuses X, unfortunately
-                pure True
-            Released -> do
-                n <- deviceName dev
-                callProcess "xinput" ["enable", C.unpack n]
-                -- ungrabDevice dev
-                pure False
-            Repeated ->
-                continue
-    KeyEvent k t -> do
-        --TODO there are some unsafe int conversions here
-        when active $ sendEvent k t
-        continue
+f sock addr s dev = \case
+    KeyEvent key eventVal ->
+        if key == KeyRightalt
+            then case eventVal of
+                Pressed -> pure if s ^. #active then s & #interrupted .~ False & #hangingSwitch .~ True else s
+                Released ->
+                    if s ^. #interrupted && s ^. #active
+                        then sendKey key eventVal >> pure s
+                        else do
+                            let active' = not $ s ^. #active
+                            devName <- deviceName dev
+                            s' <-
+                                if active'
+                                    then do
+                                        --TODO grab all
+                                        callProcess "xinput" ["disable", C.unpack devName] --TODO ByteString would be nice
+                                        C.putStrLn "Switched to active"
+                                        pure s
+                                    else do
+                                        --TODO ungrab all
+                                        callProcess "xinput" ["enable", C.unpack devName]
+                                        C.putStrLn "Switched to idle"
+                                        pure $ s & #hangingSwitch .~ False
+                            pure $ s' & #active .~ active'
+                Repeated -> continue
+            else
+                if s ^. #active
+                    then do
+                        when (s ^. #hangingSwitch) $ sendKey KeyRightalt Pressed
+                        sendKey key eventVal
+                        pure $ s & #interrupted .~ True & #hangingSwitch .~ False
+                    else continue
     _ -> continue
   where
-    continue = pure active -- don't change whether we are active
-    sendEvent k t = void $ sendTo sock (B.pack [fromIntegral $ fromEnum k, fromIntegral $ fromEnum t]) addr
+    sendKey k t = void $ sendTo sock (B.pack [fromIntegral $ fromEnum k, fromIntegral $ fromEnum t]) addr
+    continue = pure s -- don't change state
 
 allEvs :: IsStream t => t IO (Device, Event)
 allEvs = readEventsMany $ allDevices <> newDevices
